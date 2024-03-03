2587621602b973bb61ba71cadb1715c556f44f16
aleksifuna <aleksifuna@gmail.com>
2024-03-03 20:25:50 +0000

    Added route to events

---
+++ api/v1/views/event_route.py
@@ -0,0 +1,81 @@
+"""module suppies routes for user resource"""
+from models.event import Event
+from models.host import Host
+from . import db
+from datetime import datetime
+
+from flask import Blueprint, request, jsonify
+
+event_bp = Blueprint('event_bp', __name__)
+
+
+@event_bp.route('/events', methods=['GET'], strict_slashes=False)
+def return_events():
+    """
+    Returns a dictionary of lists of events in the database
+    """
+    events = Event.query.all()
+    events_data = [event.todict() for event in events]
+    return jsonify(events_data)
+
+@event_bp.route('/events/<event_id>', methods=['GET'], strict_slashes=False)
+def return_event(event_id):
+    """
+    Return a dictionary of an event details based on the event id
+    """
+    event = Event.query.filter(Event.id == event_id).first()
+    if event:
+        return jsonify(event.todict()), 200
+    else:
+        return jsonify({'error': 'Event not found'}), 404
+
+@event_bp.route('/hosts/<host_id>/events', methods=['POST'], strict_slashes=False)
+def create_event(host_id):
+    """
+    creates an event
+    """
+    host = Host.query.filter(Host.id == host_id).first()
+    if not host:
+        return jsonify({'error': 'Host not found'}), 404
+    data = request.json
+    if not data:
+        return jsonify({'error': 'Not a JSON'})
+    required = ['name', 'city', 'date', 'time', 'venue']
+    for attribute in required:
+        if attribute not in data:
+            return jsonify({'error': f'Missing {attribute}'}), 404
+    data['host_id'] = host_id
+    event = Event(**data)
+    db.session.add(event)
+    db.session.commit()
+    return event.todict(), 201
+
+@event_bp.route('/events/<event_id>', methods=['PUT'], strict_slashes=False)
+def update_event(event_id):
+    """
+    Updates an event
+    """
+    event = Event.query.filter(Event.id == event_id).first()
+    if not event:
+        return jsonify({'error': 'Event not found'}), 404
+    data = request.json
+    if not data:
+        return jsonify({'error': 'Not a JSON'}), 404
+    for key, value in data.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(event, key, value)
+    setattr(event, 'updated_at', datetime.utcnow())
+    db.session.commit()
+    return event.todict(), 201
+
+@event_bp.route('events/<event_id>', methods=['DELETE'], strict_slashes=False)
+def delete_event(event_id):
+    """
+    deletes an event
+    """
+    event = Event.query.filter(Event.id == event_id).first()
+    if not event:
+        return jsonify({'error': 'Event not found'}), 404
+    db.session.delete(event)
+    db.session.commit()
+    return jsonify({}), 201
--- models/basemodel.py
+++ models/basemodel.py
@@ -7,11 +7,7 @@ from uuid import uuid4
 class BaseModel():
     """ Base model class representation defining common attribute """
 
-<<<<<<< HEAD
-    id = db.Column(db.String(60), primary_key=True, nullable=False, default=str(uuid4()))
-=======
     id = db.Column(db.String(60), primary_key=True, nullable=False)
->>>>>>> 1e99b1eff1f53e6358dd06a0a1ea29888297f854
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 

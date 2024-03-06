05e68bb7d60ff3302a18962505a530f8208a30e4
Alex Sifuna <aleksifuna@gmail.com>
2024-03-06 22:40:27 +0300

    added js for sign in and up

---
+++ web_static/scripts/signUp.js
@@ -0,0 +1,49 @@
+function signUp() {
+    const userType = $('input[name=user-type]:checked').data('type');
+    const name =  $('#firstName').val() + ' ' + $('#lastName').val();
+    const confirmPassword = $('#confirmPassword').val();
+    const formData = {
+        name: name,
+        email: $('#email').val(),
+        phone: $('#phone').val(),
+        password: $('#password').val(),
+    };
+    console.log(formData.email);
+    console.log(formData.password);
+
+
+    if (formData.password !== confirmPassword) {
+        alert("Passwords do not match. Please re-enter your password.");
+        return;
+    }
+
+    const endPoint = getEndpoint(userType);
+    $.ajax({
+        type: 'POST',
+        url: endPoint,
+        data: JSON.stringify(formData),
+        contentType: 'application/json',
+        success: function(response) {
+            alert("Account created, Please sign-in,");
+            window.location.href = 'sign-in.html';
+        },
+        error: function(response) {
+            alert(response.responseJSON.Status);
+        }
+
+    });
+}
+
+
+function getEndpoint(userType) {
+    if (userType === 'user') {
+        return 'http://127.0.0.1:5000/api/v1/users';
+    } else if (userType === 'host') {
+        return 'http://127.0.0.1:5000/api/v1/hosts';
+    }
+}
+
+$('#signUp').submit((event) => {
+    event.preventDefault();
+    signUp();
+})

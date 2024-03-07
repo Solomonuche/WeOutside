5a2017c824810f6c68eb35a217efa97baa0db5e7
Alex Sifuna <aleksifuna@gmail.com>
2024-03-07 12:04:51 +0300

    added some login function and logout button on user-dash

--- web_static/scripts/userDash.js
+++ web_static/scripts/userDash.js
@@ -9,4 +9,19 @@ $(document).ready(function(){
             $('#eMail').text(response.email);
         }
     });
+    $('#signOut').on('click', function(){
+        $.ajax({
+            type: 'GET',
+            url: 'http://127.0.0.1:5000/api/v1/users/logout',
+            success: function() {
+                localStorage.removeItem('user_id');
+                window.location.href = 'index.html';
+            },
+            error: function(xhr, status, error) {
+                console.error('Error during logout:', error);
+                console.log(xhr);
+                console.log(status);
+            }
+        });
+    });
 });

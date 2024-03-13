b605ccf4893371c64baf875ed0930b4c0156da5a
solomonuche <solomonuche@gmail.com>
2024-03-13 04:19:34 +0100

    added host event tab and event deletion for host

--- web_static/scripts/hostDash.js
+++ web_static/scripts/hostDash.js
@@ -18,6 +18,25 @@ $(document).ready(function(){
             window.location.href = 'sign-in.html';
         }
     });
+
+    const hostEvents = 'http://127.0.0.1:5000/api/v1/hosts/' + hostId + '/myevents';
+    // custom function to loop through event objs and add them to the dom using mustache js
+    function addEvent(response, location, template) {
+        $.each(response, function (index, value) {
+        let text = Mustache.render(template, value);
+        location.append(text);
+        });
+    };
+
+    // populate my events tab for the host
+    $.get(hostEvents, function (response) {
+        let location = $('#menu1');
+        let template = $('#template').html();
+        addEvent(response, location, template);
+    });
+
+    
+
     $('#signOut').on('click', function(){
         $.ajax({
             type: 'GET',
@@ -35,3 +54,22 @@ $(document).ready(function(){
         });
     });
 });
+
+// delete event from host event tab 
+function deleteEvent(eventID) {
+    const deleteEndpoint = 'http://127.0.0.1:5000/api/v1/events/' + eventID;
+    $.ajax({
+        type: 'DELETE',
+        url: deleteEndpoint,
+        xhrFields: {
+            withCredentials: true
+        },
+        success: function(response) {
+            alert('Event deleted successfully');
+            window.location.reload();
+        },
+        error: function () {
+            alert('Error deleting event');
+        }
+    });
+}

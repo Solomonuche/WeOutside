b8a517c5c7e45512586b8eb14aca649dbc5d728b
Alex Sifuna <aleksifuna@gmail.com>
2024-03-24 01:54:07 +0300

    added functionality to send reviews and follow hosts

--- web_static/scripts/event-page.js
+++ web_static/scripts/event-page.js
@@ -1,3 +1,17 @@
+function addEvent(response, location, template, count = 0) {
+  // custom function to loop through event objs and add them to the dom using mustache js
+  let i = 0;
+  $.each(response, function (index, value) {
+    // count determine how many times the event object is added to the DOM
+    if (count !== 0 && i === count) {
+      return false;
+    };
+    let text = Mustache.render(template, value);
+    location.append(text);
+    i++;
+  });
+}
+
 $(function () {
   // Event page handling jquery
   let queryParams = new URLSearchParams(window.location.search);
@@ -33,7 +47,13 @@ $(function () {
   } else {
     $('.defualt').css("display", "block");
   }
+  let reviewUrl = `http://127.0.0.1:5000/api/v1/events/${eventID}/reviews`;
+  $.get(reviewUrl, function (response) {
+    let temp = $('#comments').html();
+    let loc = $('#home');
+    addEvent(response, loc, temp, count=4);
 
+  });
   $('#navbar').on('click', '#signOut', function(){
     $.ajax({
         type: 'GET',
@@ -50,4 +70,34 @@ $(function () {
         }
     });
   });
+  function comment() {
+    const formData = {
+        description: $('#about').val()
+    };
+    let queryParams = new URLSearchParams(window.location.search);
+    let eventID = queryParams.get('eventID');
+    const endPoint = `http://127.0.0.1:5000/api/v1/events/${eventID}/reviews`;
+    $.ajax({
+        type: 'POST',
+        url: endPoint,
+        data: JSON.stringify(formData),
+        contentType: 'application/json',
+        xhrFields: {
+            withCredentials: true
+        },
+        success: function(response) {
+            window.location.reload();
+        },
+        error: function(response) {
+  
+            alert(response.responseJSON.Status);
+        }
+  
+    });
+  }
+  
+  $('#comment-form').submit((event) => {
+    event.preventDefault();
+    comment();
+  })
 });

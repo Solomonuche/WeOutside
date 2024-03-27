2b628260ca510846c3abe80d534e9ec84402a21e
solomonuche <solomonuche@gmail.com>
2024-03-27 14:51:59 +0100

    fixed follow btn event

--- web_static/scripts/sidebar.js
+++ web_static/scripts/sidebar.js
@@ -1,21 +1,50 @@
+// add host names to host follow tab (top-3)
+function loadHostNames(req_url) {
+  $.ajax({
+    url: 'follow_template.html',
+    dataType: 'html',
+    success: function(hostTemplate) {
+      $.ajax({
+        type: 'GET',
+        url: req_url,
+        xhrFields: {
+          withCredentials: true
+        },
+        success: function(response) {
+          let location = $('#follow_tab');
+          addEvent(response, location, hostTemplate, count=3);
+        }
+      });
+    }
+  });
+}
+
+// custom function to loop through event objs and add them to the dom using mustache js
+function addEvent(response, location, template, count = 0) {
+  let i = 0;
+  $.each(response, function (index, value) {
+    // count determine how many times the event object is added to the DOM
+    if (count !== 0 && i === count) {
+      return false;
+    };
+    let text = Mustache.render(template, value);
+    location.append(text);
+    i++;
+  });
+};
+
 $(function () {
     // function to make api calls to events and populate the Dom with event listings
-    const hostUrl = 'http://127.0.0.1:5000/api/v1/hosts'; 
+    let userId = localStorage.getItem('user_id');
+    let hostUrl;
+    // check if user is sign in and return hosts user is not following
+    // else return the unsorted host list
+    if (userId === null) {
+      hostUrl = `http://127.0.0.1:5000/api/v1/hosts`;
+    } else {
+      hostUrl = `http://127.0.0.1:5000/api/v1/users/${userId}/notfollowings`;
+    } 
     const eventUrl = 'http://127.0.0.1:5000/api/v1/events';
-  
-    // custom function to loop through event objs and add them to the dom using mustache js
-    function addEvent(response, location, template, count = 0) {
-      let i = 0;
-      $.each(response, function (index, value) {
-        // count determine how many times the event object is added to the DOM
-        if (count !== 0 && i === count) {
-          return false;
-        };
-        let text = Mustache.render(template, value);
-        location.append(text);
-        i++;
-      });
-    };
   
     // add event names to popular tab (top-3)
     $.get(eventUrl, function (response) {
@@ -25,19 +54,35 @@ $(function () {
     });
   
     // add host names to host follow tab (top-3)
-    $.get(hostUrl, function (response) {
-      let location = $('#follow_tab');
-      let template = $('#follow_temp').html();
-      addEvent(response, location, template, count=3);
-    });
+    loadHostNames(hostUrl);
   
     // follow button event
     $('#follow_tab').on('click', '.follow_btn', function () {
-      if ($(this).text() === 'follow') {
-        $(this).text('followed');
-      } else {
-        $(this).text('follow');
+      if (userId === null) {
+        alert('Sign in with a User account to follow Host. Don\'t have an account? Pls sign up')
+        return;
+      }
+      const postUrl = `http://127.0.0.1:5000/api/v1/users/${userId}/followings`;
+      const requestData = {
+        host_id: $(this).data('host-id')
     };
+      $.ajax({
+        type: 'POST',
+        url: postUrl,
+        xhrFields: {
+          withCredentials: true
+        },
+        data: JSON.stringify(requestData),
+        contentType: 'application/json',
+        success: function(response) {
+          $('#follow_tab').text('');
+          loadHostNames(hostUrl);
+        },
+        error: function(textStatus, errorThrown) {
+          console.error('AJAX error:', textStatus, errorThrown);
+          window.location.href = 'sign-in.html';
+        }
+      });
     });
     
   });
